f57a20889e628608641ae39befc1abb39a77e004
Diana Holik <diana_holik@epam.com>
2021-02-22 01:27:14 +0200

    expected data was moved to the file

--- tests/specs/mainSpec.js
+++ tests/specs/mainSpec.js
@@ -2,35 +2,36 @@ const { BigBusTourPage } = require('../utils/pages/bigBusTourPage');
 const { DemoUserPage } = require('../utils/pages/demoUserPage');
 const { LoginPage } = require('../utils/pages/loginPage');
 const { MainPage } = require('../utils/pages/mainPage');
+const {expectedData} = require('../utils/expectedData');
+
+const mainPage = new MainPage();
+const loginPage = new LoginPage();
+const bigBusTourPage = new BigBusTourPage();
+const demoUserPage = new DemoUserPage();
 
 describe('e2e logining, searching and booking tour test', () => {
-    const mainPage = new MainPage();
-    const loginPage = new LoginPage();
-    const bigBusTourPage = new BigBusTourPage();
-    const demoUserPage = new DemoUserPage();
-
     it('check login', async () => {
         await mainPage.navigate();
         await mainPage.accountMenuDropDown.clickActiveItem();        
-        await expect(loginPage.getCurrentUrl()).toContain('/login');
+        await expect(loginPage.getCurrentUrl()).toContain(expectedData.loginPageUrl);
         await loginPage.login.sendKeys(browser.params.CREDENTIALS.email);
         await loginPage.password.sendKeys(browser.params.CREDENTIALS.password);
         await loginPage.loginButton.click();
-        await expect(demoUserPage.titleHello.getText()).toContain("Hi, Demo User");
+        await expect(demoUserPage.titleHello.getText()).toContain(expectedData.demoUserPageTitle);
     })
     it('check the search for the tour', async () => {
         await mainPage.navigate();
         await mainPage.toursMenuItem.click();
         await expect(mainPage.toursMenuItem.get().getAttribute('class')).toContain('active');
         await mainPage.enabletoSetValueToDestinationInput();
-        await mainPage.destinationInput.sendKeys("Big Bus Tour Of Dubai");
+        await mainPage.destinationInput.sendKeys(expectedData.tourDestination);
         await mainPage.destinationSearchResult.click();
-        await expect(mainPage.destinationSearchResultText.getText()).toEqual('Big Bus Tour Of Dubai');
+        await expect(mainPage.destinationSearchResultText.getText()).toEqual(expectedData.tourDestination);
         await mainPage.chooseTourTypeList.click();
         await mainPage.chooseTourTypeItemFerry.click();
-        await expect(mainPage.chooseTourTypeList.getText()).toEqual('Ferry');
-        await mainPage.dateInput.sendKeys('06.05.2021');
-        await expect(mainPage.dateInput.getAttribute('value')).toEqual('06.05.2021');
+        await expect(mainPage.chooseTourTypeList.getText()).toEqual(expectedData.tourType);
+        await mainPage.dateInput.sendKeys(expectedData.tourDate);
+        await expect(mainPage.dateInput.getAttribute('value')).toEqual(expectedData.tourDate);
         await mainPage.addAdultButton.click();
         await mainPage.searchButton.click();
     })
@@ -38,14 +39,14 @@ describe('e2e logining, searching and booking tour test', () => {
         await bigBusTourPage.cookiesModalBlock.dismiss();
         await bigBusTourPage.bookTourButton.moveToElement(); 
         await bigBusTourPage.bookTourButton.click();
-        await bigBusTourPage.tourBookingForm.firstnameFormBookTour.sendKeys('Diana');
-        await expect(bigBusTourPage.tourBookingForm.firstnameFormBookTour.getAttribute('value')).toEqual('Diana');
-        await bigBusTourPage.tourBookingForm.emailFormBookTour.sendKeys('dianapenfriend@yahoo.com');
-        await expect(bigBusTourPage.tourBookingForm.emailFormBookTour.getAttribute('value')).toEqual('dianapenfriend@yahoo.com');
-        await bigBusTourPage.tourBookingForm.phoneFormBookTour.sendKeys('+380501234567');
-        await expect(bigBusTourPage.tourBookingForm.phoneFormBookTour.getAttribute('value')).toEqual('+380501234567');
-        await bigBusTourPage.tourBookingForm.addressFormBookTour.sendKeys('Kiev');
-        await expect(bigBusTourPage.tourBookingForm.addressFormBookTour.getAttribute('value')).toEqual('Kiev');
+        await bigBusTourPage.tourBookingForm.firstnameFormBookTour.sendKeys(expectedData.firstnameFormBookTour);
+        await expect(bigBusTourPage.tourBookingForm.firstnameFormBookTour.getAttribute('value')).toEqual(expectedData.firstnameFormBookTour);
+        await bigBusTourPage.tourBookingForm.emailFormBookTour.sendKeys(expectedData.emailFormBookTour);
+        await expect(bigBusTourPage.tourBookingForm.emailFormBookTour.getAttribute('value')).toEqual(expectedData.emailFormBookTour);
+        await bigBusTourPage.tourBookingForm.phoneFormBookTour.sendKeys(expectedData.phoneFormBookTour);
+        await expect(bigBusTourPage.tourBookingForm.phoneFormBookTour.getAttribute('value')).toEqual(expectedData.phoneFormBookTour);
+        await bigBusTourPage.tourBookingForm.addressFormBookTour.sendKeys(expectedData.addressFormBookTour);
+        await expect(bigBusTourPage.tourBookingForm.addressFormBookTour.getAttribute('value')).toEqual(expectedData.addressFormBookTour);
         await bigBusTourPage.tourBookingForm.buttonOkFormBookTour.click();
     })
 })
